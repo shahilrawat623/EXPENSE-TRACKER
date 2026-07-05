d71b17d834b3cbe882959deead90a66dabb843a3
shahil rawat <shahilr623@gmail.com>
2026-07-05 23:41:13 +0530

    Select dynamic elements using DOM in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amt");
+const expenseAmountEl = document.getElementById("expense-amt");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("Transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
